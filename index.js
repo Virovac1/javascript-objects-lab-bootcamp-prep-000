a5db15f2bbe81b7f61f8ad69ea97b2ba56d3110a
William Spangler <bspangs@hotmail.com>
2017-12-15 19:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes= {}; //not sure if what was asked for but this empty object seems to
 //returns a clone object with the orignal key value pairs and the new key value pairs
 //but can't this only add one key-pair at a time?
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, {object},{ [key]:value })
+  return Object.assign({object},{ [key]:value })
 }
  
